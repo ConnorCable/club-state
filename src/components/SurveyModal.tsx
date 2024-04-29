2f348c0da9795f2e573db0ac43dd887544db8c4e
ConnorCable <ccab0528@gmail.com>
2024-04-28 18:25:56 -0700

    added simple cards and buttons

--- src/components/SurveyModal.tsx
+++ src/components/SurveyModal.tsx
@@ -1,6 +1,10 @@
 import {
   IonButton,
   IonButtons,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
   IonContent,
   IonHeader,
   IonModal,
@@ -9,15 +13,77 @@ import {
   IonToolbar,
 } from "@ionic/react";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import './components.css'
 export const SurveyModal: React.FC = () => {
   return (
-    <IonPage>
+    <IonPage className="">
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Survey</IonTitle>
+          <IonTitle>Your Club State</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>Hello</IonContent>
+      <IonContent className="centeredSurvey">
+            <IonCardContent>
+                <Swiper>
+                    <SwiperSlide className="surveyCard">
+                        <IonCard>
+                            <IonCardHeader className="ion-text-center">
+                                How clean was the club?
+                            </IonCardHeader>
+                            <IonCardContent className="ion-text-center">
+                                <IonButton color="danger">
+                                    Dirty
+                                </IonButton>
+                                <IonButton color="warning">
+                                    Decent
+                                </IonButton>
+                                <IonButton color="success">
+                                    Clean
+                                </IonButton>
+                            </IonCardContent>
+                        </IonCard>
+                    </SwiperSlide >
+                    <SwiperSlide className="surveyCard">
+                    <IonCard>
+                            <IonCardHeader className="ion-text-center">
+                                How clean was the club?
+                            </IonCardHeader>
+                            <IonCardContent className="ion-text-center">
+                                <IonButton color="danger">
+                                    Dirty
+                                </IonButton>
+                                <IonButton color="warning">
+                                    Decent
+                                </IonButton>
+                                <IonButton color="success">
+                                    Clean
+                                </IonButton>
+                            </IonCardContent>
+                        </IonCard>
+                    </SwiperSlide>
+                    <SwiperSlide className="surveyCard">
+                    <IonCard>
+                            <IonCardHeader className="ion-text-center">
+                                How clean was the club?
+                            </IonCardHeader>
+                            <IonCardContent className="ion-text-center">
+                                <IonButton color="danger">
+                                    Dirty
+                                </IonButton>
+                                <IonButton color="warning">
+                                    Decent
+                                </IonButton>
+                                <IonButton color="success">
+                                    Clean
+                                </IonButton>
+                            </IonCardContent>
+                        </IonCard>
+                    </SwiperSlide>
+                </Swiper>
+            </IonCardContent>
+      </IonContent>
     </IonPage>
   );
 };
