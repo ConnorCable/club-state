76041e91c2e22a44902ce3f6a78feb33268b3d68
cabmeron <116330722+cabmeron@users.noreply.github.com>
2024-07-18 21:48:37 -0700

    floating tab bar for Safari / Chrome

--- src/App.tsx
+++ src/App.tsx
@@ -131,7 +131,7 @@ const App: React.FC = () => {
                 <Redirect to="/tab1" />
               </Route>
             </IonRouterOutlet>
-            <IonTabBar slot="top" style={{ position: 'fixed', bottom: "0px", width: '100%' }}>
+            <IonTabBar className="ion-padding-bottom footer-padding" slot="bottom"style={{ position: 'fixed', bottom: "0px", width: '100%' }}>
               <IonTabButton tab="tab1" href="/tab1">
                 <IonIcon icon={homeOutline} />
                 <IonLabel>Home</IonLabel>
