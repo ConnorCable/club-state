2518161d7700d542dd9316bd9afdcf04f715ba06
cabmeron <116330722+cabmeron@users.noreply.github.com>
2024-07-18 17:52:42 -0700

    memoized map markers

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,108 +1,116 @@
-import React, { useEffect, useState } from 'react';
-import Map, {Marker, Popup} from 'react-map-gl';
-import { accessibilityOutline, homeOutline, homeSharp, locationOutline, navigateCircleOutline, person, pinSharp } from 'ionicons/icons';
-import { IonIcon, IonProgressBar, IonCard, IonCardContent, IonCardTitle, IonCardSubtitle, IonButton } from '@ionic/react';
+import React, { useEffect, useState, useMemo, useCallback } from 'react';
+import Map, { Marker, Popup } from 'react-map-gl';
+import { pinSharp, navigateCircleOutline } from 'ionicons/icons';
+import { IonIcon, IonProgressBar, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton } from '@ionic/react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import * as geofirestore from 'geofirestore';
 import { useDataStore } from '../../models/DataStore';
 import logo from "../../../assets/clubStateLogo.gif";
-import MapCard from './MapCard';
 import './index.css'
 
 const MapGL: React.FC = () => {
   const [locationChips, setLocationChips] = useState<any>([]);
-  const { location, radius} = useDataStore();
+  const { location, radius } = useDataStore();
   const [isLoading, setIsLoading] = useState(true);
   const [popupInfo, setPopupInfo] = useState<any>(null);
   const [mapLoaded, setMapLoaded] = useState(false);
 
-  const getChipCollection = async () => {
-      const firestore = firebase.firestore();
-      const GeoFirestore = geofirestore.initializeApp(firestore);
-      const geocollection = GeoFirestore.collection('geo-clubs');
-      const query = geocollection.near({ center: new firebase.firestore.GeoPoint(location!.coords.latitude, location!.coords.longitude), radius: radius });
+  const getChipCollection = useCallback(async () => {
+    const firestore = firebase.firestore();
+    const GeoFirestore = geofirestore.initializeApp(firestore);
+    const geocollection = GeoFirestore.collection('geo-clubs');
+    const query = geocollection.near({ center: new firebase.firestore.GeoPoint(location!.coords.latitude, location!.coords.longitude), radius: radius }).limit(50); // Limit added
 
-      let chipsArray: any[] = [];
-      const snapshot = await query.get();
-      snapshot.docs.forEach((doc: any) => {
-          chipsArray.push(doc.data());
-      });
-
-      return chipsArray;
-  }
+    const snapshot = await query.get();
+    return snapshot.docs.map((doc: any) => doc.data());
+  }, [location, radius]);
 
   useEffect(() => {
-      const fetchData = async () => {
-          let chips = await getChipCollection();
-          setLocationChips(chips);
-          setIsLoading(false);
-      };
+    const fetchData = async () => {
+      const chips = await getChipCollection();
+      setLocationChips(chips);
+      setIsLoading(false);
+    };
 
-      fetchData();
-  }, [location]);
+    fetchData();
+  }, [getChipCollection]);
 
-  const handleMapLoad = () => {
-    
+  const handleMapLoad = useCallback(() => {
     setMapLoaded(true);
-  };
+  }, []);
+
+  const memoizedMarkers = useMemo(() => 
+    locationChips.map((chip: any) => (
+      <Marker 
+        key={chip.g.geohash} 
+        longitude={chip.coordinates._long} 
+        latitude={chip.coordinates._lat} 
+        anchor="bottom"
+        onClick={(e) => {e.originalEvent.stopPropagation(); setPopupInfo(chip)}}
+      >
+        <IonIcon icon={pinSharp} size="large" color='tertiary' style={{ cursor: 'pointer' }} />
+      </Marker>
+    ))
+  , [locationChips]);
 
   if (isLoading) {
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '70vh' }}>
-          <img src={logo} style={{width:  500, height: 500}}></img>
-          <IonProgressBar type="indeterminate"/>
-        </div>
-      );
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '70vh' }}>
+        <img src={logo} alt="Loading" style={{width: 500, height: 500}} />
+        <IonProgressBar type="indeterminate" />
+      </div>
+    );
   }
 
   return (
-      <div id="map-container" style={{ height: "85vh", width: "90vh", visibility: mapLoaded ? 'visible' : 'hidden'}}>
-          <Map
-              mapboxAccessToken="pk.eyJ1IjoibXVra29pIiwiYSI6ImNsdng1bTZwczBnbWoydm82bTE1MXN5YmEifQ.jVrPQmWmp5xMxQamxdASVA"
-              initialViewState={{
-                  longitude: location?.coords.longitude,
-                  latitude: location?.coords.latitude,
-                  zoom: 12.5
-              }}
-              mapStyle={"mapbox://styles/mukkoi/clvx641jj01qd01q1dh0074ny"}
-              scrollZoom={true}
-              onRender={(event) => event.target.resize()}
-              onLoad={handleMapLoad}
+    <div id="map-container" style={{ height: "80vh", width: "90vh", visibility: mapLoaded ? 'visible' : 'hidden'}}>
+      <Map
+        mapboxAccessToken="pk.eyJ1IjoibXVra29pIiwiYSI6ImNsdng1bTZwczBnbWoydm82bTE1MXN5YmEifQ.jVrPQmWmp5xMxQamxdASVA"
+        initialViewState={{
+          longitude: location?.coords.longitude,
+          latitude: location?.coords.latitude,
+          zoom: 12.5
+        }}
+        mapStyle="mapbox://styles/mukkoi/clvx641jj01qd01q1dh0074ny"
+        scrollZoom={true}
+        onRender={(event) => event.target.resize()}
+        onLoad={handleMapLoad}
+      >
+        {mapLoaded && memoizedMarkers}
+        {popupInfo && (
+          <Popup
+            anchor='top'
+            longitude={popupInfo.coordinates._long}
+            latitude={popupInfo.coordinates._lat}
+            closeButton={true}
+            closeOnClick={false}
+            onClose={() => setPopupInfo(null)}
+            className='mapPinPopup'
           >
-            
-              {mapLoaded && locationChips.map((chip: any) => (
-                  <Marker key={chip.g.geohash} longitude={chip.coordinates._long} latitude={chip.coordinates._lat} anchor="bottom"  onClick={(e) => {e.originalEvent.stopPropagation; setPopupInfo(chip)}}>
-                      <IonIcon icon={pinSharp} size="large" color='tertiary' style={{ cursor: 'pointer' }} />
-                  </Marker>
-              ))}
-                {popupInfo && (
-                    <Popup
-                        anchor='top'
-                        longitude={popupInfo.coordinates._long}
-                        latitude={popupInfo.coordinates._lat}
-                        closeButton={true}
-                        closeOnClick={false}
-                        onClose={() => setPopupInfo(null)}
-                        className='mapPinPopup'
-                    >
-                        <IonCardTitle className='mapPinSubtitle'>{popupInfo.name}</IonCardTitle>
-                        <IonCardSubtitle>{popupInfo.address.toUpperCase()}</IonCardSubtitle>
-                        <IonCardContent>
-                            
-                            <IonButton className = "getDirections" color={"warning"}  >
-                                <a className='directionsButton'  href={"https://www.google.com/maps/dir/?api=1&destination=" + popupInfo.coordinates._lat + "," + popupInfo.coordinates._long} target="_blank" rel="noreferrer"> <h2>Get Directions</h2></a>
-                            </IonButton>
-                        </IonCardContent>
-                    </Popup>
-                )}
-              {
-              (mapLoaded && location?.coords.longitude != undefined && location.coords.latitude != undefined) ? <Marker latitude={location.coords.latitude} longitude={location.coords.longitude}> <IonIcon icon={navigateCircleOutline} size="large" color='tertiary' /></Marker>  : <></>
-              } 
-
-          </Map>
-                 
-      </div>
+            <IonCardTitle className='mapPinSubtitle'>{popupInfo.name}</IonCardTitle>
+            <IonCardSubtitle>{popupInfo.address.toUpperCase()}</IonCardSubtitle>
+            <IonCardContent>
+              <IonButton className="getDirections" color="warning">
+                <a 
+                  className='directionsButton' 
+                  href={`https://www.google.com/maps/dir/?api=1&destination=${popupInfo.coordinates._lat},${popupInfo.coordinates._long}`} 
+                  target="_blank" 
+                  rel="noreferrer"
+                >
+                  <h2>Get Directions</h2>
+                </a>
+              </IonButton>
+            </IonCardContent>
+          </Popup>
+        )}
+        {mapLoaded && location?.coords.longitude != undefined && location.coords.latitude != undefined && (
+          <Marker latitude={location.coords.latitude} longitude={location.coords.longitude}>
+            <IonIcon icon={navigateCircleOutline} size="large" color='tertiary' />
+          </Marker>
+        )}
+      </Map>
+    </div>
   );
 };
 
