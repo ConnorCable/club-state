43625c92bb4aae161f2f32725c8ff491f7f36747
cabmeron <116330722+cabmeron@users.noreply.github.com>
2024-08-04 16:10:11 -0700

    slightly lighter genre button background gradient

---
+++ src/components/MapMarkerModal/index.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import {
+    IonModal,
+    IonContent,
+    
+} from "@ionic/react";
+
+
+const MapMarkerModal: React.FC<any> = () => {
+    
+    return (<></>);
+}
